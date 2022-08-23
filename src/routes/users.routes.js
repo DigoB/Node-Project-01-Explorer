851ebd31218bb68e6119a49c546f6bdc83708e53
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-23 15:20:54 -0300

    Using middleware to simulate autentication

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,8 +3,28 @@ const { Router } = require("express")
 const UsersController = require("../controllers/UsersController")
 const usersRoutes = Router()
 
+/** 
+ * O Middleware é um passo antes de chegar a função de destino, podemos utilizar ele para fazer
+ * algum tipo de tratamento dos dados antes chegar a função e ser executada.
+ * Pode ser usado por exemplo para um fluxo de autenticação.
+ */
+function middleware(request, response, next) {
+
+    console.log("Voce passou pelo midleware")
+    
+    if(!request.body.isAdmin) {
+        return response.status(401).json({ message: "User unauthorized"})
+    }
+    
+    next()
+}
+
 const usersController = new UsersController()
 
-usersRoutes.post("/users", usersController.create)
+/** Caso deseje utilizar o middleware para todas as rotas podemos aplica-lo da seguinte forma:
+ * usersRoutes.use(middleware)
+ * Para isso é necessário analisar se faz sentido a mesma regra para todos os endpoints
+ */
+usersRoutes.post("/users", middleware, usersController.create)
 
 module.exports = usersRoutes;
