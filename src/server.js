baadbfcf2bb39c4132b5a19a8eee1890161fa01d
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-23 11:10:48 -0300

    Endpoint GET using query params

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+const { request } = require("express");
 const express = require("express");
 
 const app = express();
 
 // Dessa forma nós declaramos parametros da requisição no Express
+// Route params são obrigatórios
 app.get("/message/:id/:user", (request, response) => {
 
     // Desestruturação
@@ -10,6 +12,15 @@ app.get("/message/:id/:user", (request, response) => {
 
     response.send(`Id da requisição: ${id}.
     Para o usuário: ${user}`)
+})
+
+/** Query Params, podemos atribuir parametros de pesquisa no endpoint sem necessáriamente
+ * eles serem obrigatórios como o Route Params
+ */
+app.get("/users", (request, response) => {
+    const { page, limit } = request.query
+
+    response.send(`Página: ${page}. Limite: ${limit}`)
 })
 
 const PORT = 3333;
