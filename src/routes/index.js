92493f94c22fed4d160fd3dcfb911fde1c84f5fb
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-08-23 12:44:02 -0300

    Module for routes, and beginning of controllers

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const {Router} = require("express")
+
+const usersRoutes = require("./users.routes")
+
+const router = Router()
+
+/** Quando a requisição vier do server.js, vai procurar qual a requisição 
+ * solicitada, nesse caso, está solicitando a /users.
+ * Ao encontrar o endereço, o use() vai direcionar para a rota que foi definida
+ * no require() acima
+ */
+router.use("/users", usersRoutes)
+
+module.exports = usersRoutes
--- src/server.js
+++ src/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
+const routes = require("./routes")
+
 const app = express();
 
 // O Express precisa saber qual o padrao de dados a ser usado
 app.use(express.json())
 
-app.post("/users", (request, response) => {
-
-    const { name, email, password } = request.body
-
-    response.json({ name, email, password })
-})
+/** Quando houver uma requisição para o servidor, vai cair aqui nesse método das rotas
+ * Ele foi definido acima para buscar as rotas na pasta ./routes, como não defini qual dos 
+ * arquivos a ser buscado, o default é ele procurar pelo index.js
+ */
+app.use(routes)
 
 const PORT = 3333;
 app.listen(PORT, () => {
