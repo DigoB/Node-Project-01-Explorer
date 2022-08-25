f994157fd47c1184a83f9908bc109325fdadb520
Rodrigo Braz <rodrigo@pop-os.localdomain>
2022-08-25 18:11:49 -0300

    Password encrypt

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,7 @@
+const {hash} = require("bcryptjs")
 const AppError = require("../utils/AppError")
-
 const sqliteConnection = require("../../database/sqlite")
+
 
 
 class UsersController {
@@ -15,7 +16,8 @@ class UsersController {
             throw new AppError("Email já cadastrado")
         }
 
-        await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)", [name, email, password])
+        const hashedPassword = await hash(password, 8)
+        await database.run("INSERT INTO users (name, email, password) VALUES (?,?,?)", [name, email, hashedPassword])
 
         return response.status(201).json()
 
